5214827f3bee008b05329cc8ad4ea73894b8d4f4
Carlos Cueto <crcueto25@gmail.com>
2023-09-14 21:10:29 +0300

    Add simple algorithm for step counting

--- stepcounter.py
+++ stepcounter.py
@@ -41,12 +41,70 @@ def read_data(filename):
 # Should return an array of timestamps from when steps were detected
 # Each value in this arrray should represent the time that step was made.
 def count_steps(timestamps, x_arr, y_arr, z_arr):
-    # TODO: Actual implementation
-    rv = []
+    # See the pdf: pedometer-design-3-axis-digital-acceler fore more details
+
+    # Declare parameters for step counting (using dynamic threshold and dynamic precision)
+    window_size = 50  # Window size for the algorithm (adjustable)
+    precision_threshold = 0.5  # Fixed precision threshold (adjustable)
+
+    # Process the data based on the window size
+    # Calculate the magnitude of the total acceleration
+    magnitude_total_acc = []
+    for x, y, z in zip(x_arr, y_arr, z_arr):
+        magnitude_total_acc.append(magnitude(x, y, z))
+    step_times = []  # Array to store the times when steps were detected
+    sample_old = magnitude_total_acc[0]  # Initialize the previous sample
+    sample_new = magnitude_total_acc[1]  # Initialize the current sample
+    sample_result = sample_old  # Initialize the sample result
+    thresholds = []  # For visualization
+    maxs = []  # For visualization
+    mins = []  # For visualization
+
+    # Iterate over the data
     for i, time in enumerate(timestamps):
-        if i == 0:
-            rv.append(time)
-    return rv
+        # Check which of the axes has the magnitude cl
+        # When a new data sample comes, sample_new is shifted to the sample_old register unconditionally
+        sample_old, sample_new = sample_new, magnitude_total_acc[i]
+        # If the changes in acceleration are greater than a predefined precision, the newest sample result, is shifted to the sample_new register; otherwise the sample_new register will remain unchanged.
+        if abs(sample_new - sample_old) > precision_threshold:
+            sample_result = sample_new
+        else:
+            sample_result = sample_old
+
+        # Calculate the maximum and minimum acceleration in the window
+        maximum = np.max(magnitude_total_acc[i : i + window_size])
+        minimum = np.min(magnitude_total_acc[i : i + window_size])
+        # Calculate the threshold for the window
+        threshold = (maximum + minimum) / 2
+        # A step is defined as happening if there is a negative slope of the acceleration plot (sample_new < sample_old) when the  acceleration curve crosses below the dynamic threshold.
+        # TODO: Check if this is correct
+        if sample_new < threshold and sample_old >= threshold:
+        # if sample_new < sample_old and sample_result < threshold:
+            step_times.append(time)
+        # Store the threshold for visualization
+        thresholds.append(threshold)
+        maxs.append(maximum)
+        mins.append(minimum)
+
+    # Plot the data
+    fig, ax = plt.subplots()
+    ax.plot(timestamps, magnitude_total_acc, color="blue", linewidth=1.0, label="data")
+    ax.plot(timestamps, thresholds, color="orange", linewidth=1.0, label="threshold")
+    ax.plot(timestamps, maxs, color="red", linewidth=1.0, label="max")
+    ax.plot(timestamps, mins, color="black", linewidth=1.0, label="min")
+    for step_time in step_times:
+        ax.axvline(x=step_time, color="green", linewidth=1.0)
+    ax.set(xlabel="time (ms)", ylabel="magnitude", title="Step counter")
+    ax.legend()
+    ax.grid()
+    plt.show()
+
+    # TODO: Actual implementation
+    # rv = []
+    # for i, time in enumerate(timestamps):
+    #     if i == 0:
+    #         rv.append(time)
+    return step_times
 
 
 def count_steps_advanced(timestamps, x_arr, y_arr, z_arr):
@@ -111,6 +169,7 @@ def main():
         # Count the steps based on array of measurements from accelerometer
         st = count_steps(timestamps, x_array, y_array, z_array)
     else:
+        print("Using advanced algorithm for step counting")
         st = count_steps_advanced(timestamps, x_array, y_array, z_array)
     # Print the result
     print(
